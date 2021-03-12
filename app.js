ce05845502b18c22c0086fd6d986b0ce2b47a522
AyrlonDouglas <ayr.l@hotmail.com>
2021-03-11 23:01:54 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,174 @@
+const express = require('express');
+const fun = require('./functions')
+const server = express();
+const port = 8000;
+
+const layoutBase = `<!DOCTYPE html>
+                        <html lang="en">
+
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <link rel="stylesheet" href="style.css">
+                            <title>minefield</title>
+                        </head>
+
+                        <body>
+                            <header>
+                                <h1>Minefield</h1>
+                                <nav>
+                                    <ul>
+                                        <li><a href="/">Start</a></li>
+                                        <li><a href="/author">Author</a></li>
+                                    </ul>
+                                </nav>
+                            </header>`
+let transparencia;
+let arrayComBombasVerificadas;
+let arraySemBombas = [];
+let arrayComBombas;
+let numeroDeBombas;
+let linhas;
+let colunas;
+let clicks;
+
+server.get('/', (req, res) => {
+    let html = layoutBase;
+    html += `
+        <a href="/level"><button  class="butto-nPlay">Play!</button></a>
+    </body></html>`
+    res.send(html)
+});
+server.get('/author', (req, res) => {
+    let html = layoutBase;
+    html += `<div class="author">
+                <h2>Ayrlon Vilarim</h2>
+                <p>Phone : +55 (81) 99275-1174</p>
+                <p>E-mail : ayr.l@hotmail.com</p>
+                <p>Instagram : @ayrlon</p>
+            </div>`
+    res.send(html)
+})
+
+server.get('/level', (req, res) => {
+    let html = layoutBase;
+    html += `<div class="level"> 
+                <ul class="level">
+                    <a href="/game?lin=9&col=9&bombas=10"><li>Easy</li></a>
+                    <a href="/game?lin=16&col=16&bombas=40"><li>Average</li></a>
+                    <a href="/game?lin=16&col=30&bombas=100"><li>Hard</li></a>
+                </ul>
+            </div>
+            </body>
+            </html>`
+
+    res.send(html)
+})
+
+server.get('/game', (req, res) => {
+    let html = layoutBase;
+    clicks = 0;
+    numeroDeBombas = req.query.bombas;
+    linhas = req.query.lin;
+    colunas = req.query.col;
+
+    arraySemBombas = fun.criarArray(linhas, colunas);
+
+    arrayComBombas = fun.implementarBombasNoArray(arraySemBombas, numeroDeBombas);
+
+    console.log(arrayComBombas)
+    arrayComBombasVerificadas = fun.verificarLocalBombas(arrayComBombas);
+
+    transparencia = fun.criarTransparencia(linhas, colunas);
+
+    html += fun.criarTabela(linhas, colunas, transparencia, arrayComBombasVerificadas);
+
+    html += `</body></html>`;
+    res.send(html);
+});
+
+server.get('/gaming', (req, res) => {
+    let html = layoutBase;
+    let lin = req.query.linha;
+    let col = req.query.coluna;
+
+    if (transparencia[lin][col] == -1) {
+        transparencia[lin][col] = -2;
+        clicks++
+    }
+    if (((colunas * linhas) - numeroDeBombas) == clicks) {
+        res.redirect('/victory')
+    }
+
+    let clicouEmBomba = fun.verificarSeTemBomba(transparencia, arrayComBombasVerificadas);
+
+    html += fun.criarTabela(linhas, colunas, transparencia, arrayComBombasVerificadas);
+
+    html += `</body></html>`;
+
+
+    if (clicouEmBomba) {
+        res.redirect('/lose1')
+    } else {
+        res.send(html)
+    }
+});
+
+server.get('/lose1', (req, res) => {
+    let html = `<!DOCTYPE html>
+                    <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <meta http-equiv="Refresh" content="2; url='/lose'" />
+                            <link rel="stylesheet" href="style.css"> 
+                            <title>minefield</title>
+                        </head>
+                        <body>
+                            <header>
+                                <h1>Minefield</h1>
+                                <nav>
+                                    <ul>
+                                        <li><a href="/">Start</a></li>
+                                    </ul>
+                                </nav>
+                            </header>`
+
+    fun.zerarTansparencia(transparencia)
+
+    html += fun.criarTabela(linhas, colunas, transparencia, arrayComBombasVerificadas);
+
+    html += `</body></html>`;
+
+    res.send(html);
+});
+
+server.get('/lose', (req, res) => {
+    let html = layoutBase;
+    html += `<div class="wrapper-lose">
+                <div class="lose"><h2>You exploded!</h2></div>
+                <a href="/level"><button>Play Again?</button></a>
+            </div>
+            </body></html>`
+    res.send(html)
+});
+
+server.get('/victory', (req, res) => {
+    let html = layoutBase;
+    html += `<div class="victory">
+                <div class="wrapper-lose">
+                <h2> You won! </h2>
+                <a href="/level"><button>Play Again?</button></a>
+            </div>
+            </div>`;
+
+    res.send(html);
+});
+
+server.use(express.static('public'));
+
+server.listen(port, () => {
+    console.log(`server ir running in port: ${port}`)
+});
---
+++ functions.js
@@ -0,0 +1,163 @@
+module.exports = {
+    criarTabela: (linhas, colunas, transparencia, bombas) => {
+        let tabela = `<div class="tabela-jogo"><table>`;
+
+        for (let i = 0; i < linhas; i++) {
+            tabela += `<tr>`
+
+            for (let j = 0; j < colunas; j++) {
+                if (transparencia[i][j] == -1) {
+
+                    tabela += `<td><a href="/gaming?linha=${i}&coluna=${j}"><div class="campo"></div></a></td>`
+                }
+                else if (bombas[i][j] == 9) {
+                    tabela += `<td><img class="img-bomba" src="images/bomb.svg"></td>`
+                } else {
+                    tabela += `<td class="campo-sem-mina">${bombas[i][j]}</td>`
+                }
+            }
+        }
+        tabela += `</table></div>`
+        return tabela
+    },
+
+    criarTransparencia: (linhas, colunas) => {
+        let arrayTransparencia = [];
+
+        for (let i = 0; i < linhas; i++) {
+            arrayTransparencia.push([]);
+            for (let j = 0; j < colunas; j++) {
+                arrayTransparencia[i].push(-1)
+            }
+        }
+        return arrayTransparencia
+    },
+
+    criarArray: (linhas, colunas) => {
+        let array = [];
+        for (let i = 0; i < linhas; i++) {
+            array.push([]);
+            for (let j = 0; j < colunas; j++) {
+                array[i].push(0)
+            }
+        }
+        return array
+    },
+
+    implementarBombasNoArray: (array, numeroBombas) => {
+        let contadorBombas = 0;
+        let arrayComBombas = array;
+
+        while (contadorBombas < numeroBombas) {
+            const linha = Math.floor(Math.random() * array.length);
+            const coluna = Math.floor(Math.random() * array[0].length)
+
+            if (arrayComBombas[linha][coluna] != 9) {
+                arrayComBombas[linha][coluna] = 9;
+                contadorBombas++
+            }
+        }
+        return arrayComBombas
+    },
+
+    verificarLocalBombas: (array) => {
+        let arrayComBombas = array;
+
+        for (let i = 0; i < arrayComBombas.length; i++) {
+            for (let j = 0; j < arrayComBombas[i].length; j++) {
+
+                let contadorBombasAoRedor = 0;
+
+                const linhaAnterior = i - 1;
+                const linhaPosterior = i + 1;
+
+                const colunaAnterior = j - 1;
+                const colunaPosterior = j + 1;
+
+                if (arrayComBombas[i][j] != 9) {
+
+                    if (linhaAnterior != -1
+                        && colunaAnterior != -1
+                        && arrayComBombas[linhaAnterior][colunaAnterior] == 9) {
+
+                        contadorBombasAoRedor++
+                    }
+                    if (linhaAnterior != -1
+                        && arrayComBombas[linhaAnterior][j] == 9) {
+
+                        contadorBombasAoRedor++
+                    }
+                    if (linhaAnterior != -1
+                        && colunaPosterior != array[i].length
+                        && arrayComBombas[linhaAnterior][colunaPosterior] == 9) {
+                        contadorBombasAoRedor++
+                    }
+                    if (colunaAnterior != -1
+                        && arrayComBombas[i][colunaAnterior] == 9) {
+                        contadorBombasAoRedor++
+                    }
+                    if (colunaPosterior != array[i].length
+                        && arrayComBombas[i][colunaPosterior] == 9) {
+
+                        contadorBombasAoRedor++
+                    }
+                    if (linhaPosterior != array.length
+                        && colunaPosterior != array[i].length
+                        && arrayComBombas[linhaPosterior][colunaAnterior] == 9) {
+
+                        contadorBombasAoRedor++
+                    }
+                    if (linhaPosterior != array.length
+                        && arrayComBombas[linhaPosterior][j] == 9) {
+
+                        contadorBombasAoRedor++
+                    }
+                    if (linhaPosterior != array.length
+                        && colunaPosterior != array[i].length
+                        && arrayComBombas[linhaPosterior][colunaPosterior] == 9) {
+                        contadorBombasAoRedor++
+                    }
+                    arrayComBombas[i][j] = contadorBombasAoRedor
+
+                }
+
+            }
+        }
+        return arrayComBombas
+    },
+
+    verificarSeTemBomba: (arrayTransparencia, arrayDeBombas) => {
+
+        for (let i = 0; i < arrayTransparencia.length; i++) {
+
+            for (let j = 0; j < arrayTransparencia[i].length; j++) {
+
+                if (arrayTransparencia[i][j] == -2) {
+                    if (arrayDeBombas[i][j] == 9) {
+                        return true
+                    }
+                }
+            }
+        } return false
+    },
+    zerarTansparencia: (array) => {
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array[i].length; j++) {
+                array[i][j] = 0
+            }
+        }
+    },
+
+    verificarVencedor: (array) => {
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array[i].length; j++) {
+                if (array[i][j] != -2) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
+
+
