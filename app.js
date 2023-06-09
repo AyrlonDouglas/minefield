1a0e6b65e64898701e890c01d452be734a7a3978
Ayrlon Vilarim <ayrlon.vilarim@salvus.me>
2023-06-08 23:53:50 -0300

    fix vercel

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const server = express();
 const path = require('path')
 // routes
-const minefieldRoutes = require('./routes/minefield.routes');
+const minefieldRoutes = require('./src/routes/minefield.routes');
 // config
 const port = 8000;
 
-server.use(express.static(path.dirname('public') +'/public'));
+server.use(express.static('public'));
 
-server.use('/api', minefieldRoutes)
+server.use('/', minefieldRoutes)
 
 server.listen(process.env.PORT || port, () => {
     console.log(`server ir running in port: ${port}`)
